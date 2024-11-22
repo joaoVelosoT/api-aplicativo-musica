f68ab602cef39e8d32ccc73cd95436d1759355c6
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-11-22 18:39:05 -0300

    mudando a config do database, para usar o database mongodb da nuvem

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const connectDB = async () => {
     try {
-        const mongoURI = "mongodb://localhost:27017/appSonora";
+
+        const HOST_DB = process.env.HOST_DB
+        const PASS_DB = process.env.PASS_DB
+        const URL_DB = process.env.URL_DB
+
+        const mongoURI = `mongodb+srv://${HOST_DB}:${PASS_DB}${URL_DB}/?retryWrites=true&w=majority&appName=database-sonora`;
         await mongoose.connect(mongoURI);
         console.log("Mongodb Conectado !");
     } catch (error) {
